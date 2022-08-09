f248d1ccd49f58ed6daa7a736e386179c4b8b0c4
Eric Wang <eric533528@gmail.com>
2022-08-09 15:46:32 +0800

    add annotation

--- product.py
+++ product.py
@@ -1,4 +1,5 @@
 product = []
+#讀取檔案
 with open('products.csv', 'r', encoding = 'utf-8-sig') as f:
 	for line in f:
 		if '商品,價格' in line:
@@ -7,6 +8,7 @@ with open('products.csv', 'r', encoding = 'utf-8-sig') as f:
 		product.append([name, price])
 print(product)
 
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -15,9 +17,11 @@ while True:
 	product.append([name, price])
 print(product)
 
+#印出所有購買紀錄
 for p in product:
 	print(p[0],'的價格是',p[1])
 
+#寫入檔案
 with open('products.csv', 'w', encoding = 'utf-8-sig') as f:
 	f.write('商品,價格\n')
 	for p in product:
