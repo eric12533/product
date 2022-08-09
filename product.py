4735c6620d84bab987a1522d7bb1cac53f56a94f
Eric Wang <eric533528@gmail.com>
2022-08-09 15:41:24 +0800

    add read file

--- product.py
+++ product.py
@@ -1,4 +1,12 @@
 product = []
+with open('products.csv', 'r', encoding = 'utf-8-sig') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name,price = line.strip().split(',')
+		product.append([name, price])
+print(product)
+
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
