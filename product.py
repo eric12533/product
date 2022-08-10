2f435531de3dce9860ca7c5dc08324296e0d3663
Eric Wang <eric533528@gmail.com>
2022-08-10 15:30:59 +0800

    refactor

--- product.py
+++ product.py
@@ -1,35 +1,50 @@
 import os
 
-product = []
-#讀取檔案
-if os.path.isfile('products.csv'):
-	print('存在檔案!')
-	with open('products.csv', 'r', encoding = 'utf-8-sig') as f:
+def read_file(filename):
+	product = []
+	with open(filename, 'r', encoding = 'utf-8-sig') as f:
 		for line in f:
 			if '商品,價格' in line:
 				continue
 			name,price = line.strip().split(',')
 			product.append([name, price])
-	print(product)
-else:
-	print('沒有檔案')
+	return product
+
 #讓使用者輸入
-while True:
-	name = input('請輸入商品名稱: ')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格: ')
-	product.append([name, price])
-print(product)
+def user_input(product):
+	while True:
+		name = input('請輸入商品名稱: ')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格: ')
+		product.append([name, price])
+	print(product)
+	return product
 
 #印出所有購買紀錄
-for p in product:
-	print(p[0],'的價格是',p[1])
+def print_product(product):
+	for p in product:
+		print(p[0],'的價格是',p[1])
+
 
 #寫入檔案
-with open('products.csv', 'w', encoding = 'utf-8-sig') as f:
-	f.write('商品,價格\n')
-	for p in product:
-		f.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename, product):
+	with open(filename, 'w', encoding = 'utf-8-sig') as f:
+		f.write('商品,價格\n')
+		for p in product:
+			f.write(p[0] + ',' + p[1] + '\n')
 
-	
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):
+		print('存在檔案!')
+		product = read_file(filename)
+	else:
+		print('沒有檔案')
+	product = user_input(product)
+	print_product(product)
+	write_file('products.csv', product)
+
+main()
+
